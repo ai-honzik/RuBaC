424cef8c70f415c51cbf35310fbde7758ce456b1
Jan Dolejs <dolejj13@fit.cvut.cz>
2021-09-02 14:58:45 +0200

    Little change

--- wrapper/RIPPER.py
+++ wrapper/RIPPER.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python5
 
 from .rbc import CRIPPER
 
 class RIPPER( CRIPPER ):
   def __init__( self, *args, **kwargs ):
-    super( RIPPER, self ).__init__( *args, **kwargs )
+    super().__init__( *args, **kwargs )
 
   def fit( self, X, Y, feature_names, positive_class ):
-    return super( RIPPER, self ).fit( X.T, Y, feature_names, positive_class )
+    return super().fit( X.T, Y, feature_names, positive_class )
